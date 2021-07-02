c62a2783d60aaa704a14426e1b6fc244c73e8921
Sajid16 <sajid.mahboob16@gmail.com>
2021-07-03 01:24:51 +0600

    first commit on 3rd july

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            string msg = "get single product of id " + id;
+            return msg;
+        }
+    }
+}
---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+
+    }
+}
